ae46d401303f019b66ad6953a1ed435075ba9925
Adam Simon <adamsimon85100@gmail.com>
2026-05-20 19:48:34 +0300

    add favicons

--- enlune-site/src/app/layout.tsx
+++ enlune-site/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist_Mono, Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
+import Head from "next/head";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -29,8 +30,16 @@ export default function RootLayout({
       lang="en"
       className={`${inter.variable} ${geistMono.variable} h-full scroll-smooth antialiased`}
     >
-      <Analytics />
-      <body className="min-h-full text-ink">{children}</body>
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="32x32" type="image/x-icon" />
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      </Head>
+
+      <body className="min-h-full text-ink">
+        <Analytics />
+        {children}
+      </body>
     </html>
   );
 }
